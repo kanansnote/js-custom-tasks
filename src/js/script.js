a7a86d844e261c2b405b5148b79835c00f73eb82
kanan <kanansnote@gmail.com>
2024-05-19 14:14:10 +0400

    Added some array tasks.

--- src/js/script.js
+++ src/js/script.js
@@ -214,8 +214,9 @@
 
 // isNumberPowed(9, 115);
 
-
-// 
+// --------------------------------------------------
+// Array Tasks
+// Task 1
 // function mixedArrtask(arr1, arr2){
 //     let joinedArrs = arr1.concat(arr2);
 //     let sortedArr = joinedArrs.sort();
@@ -224,3 +225,126 @@
 // }
 
 // console.log(mixedArrtask([5,2,3,9], [2,9,1]));
+
+// Task 2
+
+// function theSmallestNumber(arr){
+//     let sortedArr = arr.sort(function(a, b){return a - b});
+
+//     // 1, 4, 4, 7, 7
+//     // 1, 3, 3
+
+//     let newArr = [];
+
+//     for(let i = 0; i < arr.length; i++) {
+
+//         if (!(sortedArr.includes(i))) {
+//             newArr.push();
+//         }        
+//     }
+
+//     console.log(newArr);
+// }
+
+// theSmallestNumber([7,7,4,4,1]);
+// theSmallestNumber([3,3,1]);
+
+// Task 3
+// function bölünənƏdədlər(num){
+//     let 
+// }
+
+// console.log(bölünənƏdədlər(24)); // bölənlər [1,2,3,4,6,8,12,24], bölənlərin sayı - 8
+// console.log(bölünənƏdədlər(67)); // bölənlər [1,67], bölənlərin sayı - 2
+
+// Task 4
+
+// function sayiniYaziyaCevir(num) {
+//     const reqemler = [
+//         'bir',
+//         'iki',
+//         'üç',
+//         'dört',
+//         'beş',
+//         'altı',
+//         'yeddi',
+//         'səkkiz',
+//         'doqquz',
+//     ];
+
+//     const onluqlar = [
+//         'on',
+//         'iyirmi',
+//         'otuz',
+//         'qırx',
+//         'əlli',
+//         'altmış',
+//         'yetmiş',
+//         'səksən',
+//         'doxsan',
+//     ];
+
+// }
+
+
+// sayiniYaziyaCevir(67);  // altmış yeddi
+// sayiniYaziyaCevir(82);  // səksən iki
+
+
+// ChatGPT kodu
+// function bolenleriVeSayisi(n) {
+//     // 1-dən n-ə qədər olan rəqəmlərdən ibarət array yaradırıq
+//     const numbers = Array.from({ length: n }, (_, i) => i + 1);
+    
+//     // `filter` metodu ilə bölənləri tapırıq
+//     const bolenler = numbers.filter(num => n % num === 0);
+    
+//     // Nəticələri ekrana yazdırırıq
+//     console.log(`Bölənlər: ${bolenler.join(', ')}`);
+//     console.log(`Bölənlərin sayı: ${bolenler.length}`);
+// }
+
+// // Nümunə istifadə
+// bolenleriVeSayisi(12);
+
+// // Şadmanın kodu
+// function bolunenEdedler(num) {
+//     const arr = [];
+//     for (let i = 1; i <= num; i++) {
+//         if (num % i === 0) {
+//             arr.push(i);
+//         }
+//     }
+    
+//     return `tam bolenler ${arr} ve bolenlerin sayi ${arr.length}`;
+// }
+
+// console.log(bolunenEdedler(24));  // bölenler [1, 2, 3, 4, 6, 8, 12, 24] bğlenlerin sayi - 8
+// console.log(bolunenEdedler(67));  // bölenler [1, 67]
+
+// Task 5
+// const news1 =
+//     'Cüdo üzrə Azərbaycanın gənclərdən ibarət millisi İspaniyanın Malaqa şəhərində davam edən Avropa Kubokunda iki medal qazanıb.';
+// const len1 = 20;
+
+// const news2 = 'Şahdağda qarın hündürlüyü iki santimetrə çatıb';
+// const len2 = 15;
+
+// function xeberinQisaltmasi(str, par2) {
+
+//     let news = String(str);
+//     let len = Number(par2);
+
+//     // console.log(news.length);
+
+//     if (news.length > len) {
+//        let newsNewLength = news.slice(0, len);
+//        let newsEndDotts = newsNewLength.padEnd(3, '...');
+
+//        console.log(newsEndDotts);
+//     }
+
+//     // return newsEdited, len;
+// }
+// console.log(xeberinQisaltmasi(news1, len1));   // Cüdo üzrə Azərbaycanın...
+// console.log(xeberinQisaltmasi(news2, len2));   // Şahdağda qarın hündürlüyü...
