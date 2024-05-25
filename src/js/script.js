6134c25500526630d9f9eb6125fcfd831d4fca37
kanan <kanansnote@gmail.com>
2024-05-25 13:09:29 +0400

    Added Codewars related js resolved task.

--- src/js/script.js
+++ src/js/script.js
@@ -384,3 +384,15 @@
 // minMax([1, 2, 3, 4, 5]) // [1, 5]
 // minMax([2334454, 5]) // [5, 2334454]
 // minMax([1]) // [1, 1]
+
+// Codewars Tasks
+
+// const binaryArrayToNumber = arr => {
+//   const binaryString = arr.join('');
+//   const number = parseInt(binaryString, 2);
+//   return number;
+// };
+  
+// console.log(binaryArrayToNumber([0, 0, 0, 1]));
+// console.log(binaryArrayToNumber([0, 0, 1, 0]));
+// console.log(binaryArrayToNumber([0, 0, 1, 1]));
